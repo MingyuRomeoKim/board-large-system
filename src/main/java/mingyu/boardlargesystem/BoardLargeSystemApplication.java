e8dd5471e6a00c9f12b3a141dd512da831faa26a
mingyu <mingyu@mtn.co.kr>
2025-06-20 09:49:11 +0900

    spring-boot install

---
+++ src/main/java/mingyu/boardlargesystem/BoardLargeSystemApplication.java
@@ -0,0 +1,13 @@
+package mingyu.boardlargesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardLargeSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardLargeSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mingyu/boardlargesystem/BoardLargeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package mingyu.boardlargesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardLargeSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
